30ad7c5e3a2b481fb10e69acd9d40aa6a30668fa
jaydub83 <jeremy@dancinhomer.(none)>
2015-06-25 14:59:50 -0400

    test

--- discogs/js/controllers/main.js
+++ discogs/js/controllers/main.js
@@ -2,8 +2,10 @@
 
 var appControllers = angular.module('appControllers', []);
 
-appControllers.controller('detailsCtrl', ['$scope', 
-    function($scope) {
-        
+appControllers.controller('detailsCtrl', ['$scope', '$http', 
+    function($scope, $http) {
+        $http.get('https://api.discogs.com/oauth/identity').success(function(data) {
+            console.log(data);
+        });
     }
 ]);
